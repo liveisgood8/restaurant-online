267dab6b8296e6f8161120f12c5d2c026cff94d4
Kirill <lemeshonok12@gmail.com>
2020-05-03 21:13:25 +0700

    initialize application with spring initializer

---
+++ src/test/java/com/nexus/restaurantonline/RestaurantOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus.restaurantonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
