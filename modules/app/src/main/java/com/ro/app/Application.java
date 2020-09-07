d8d9e411bbd6a316ec387dc0ed95ae90610fab0b
Kirill <lemeshonok12@gmail.com>
2020-09-07 21:44:28 +0700

    enable cors

--- modules/app/src/main/java/com/ro/app/Application.java
+++ modules/app/src/main/java/com/ro/app/Application.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Import;
 		MenuModuleConfig.class,
 		OrdersServiceConfig.class,
 })
-
 public class Application {
 
 	public static void main(String[] args) {
